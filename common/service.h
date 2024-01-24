4c905850e6523e518311536b89cc4eb871c6c024
vixmt22 <157559357+vixmt22@users.noreply.github.com>
2024-01-24 06:56:19 +0300

    Add files via upload

---
+++ common/service.h
@@ -0,0 +1,76 @@
+#ifndef __INC_SERVICE_H__
+#define __INC_SERVICE_H__
+
+#define _IMPROVED_PACKET_ENCRYPTION_								//Sistemler
+#define __PET_SYSTEM__												//Sistemler
+#define __UDP_BLOCK__												//Sistemler
+#define ENABLE_FIX_READ_ETC_DROP_ITEM_FILE_BY_VNUM					//Sistemler
+#define ENABLE_SKILL_BOOK_CORE										//Sistemler
+#define ENABLE_FIX_DESTROY_GUILD_IF_WAR_IS_ACTIVE					//Sistemler
+#define FLUSH_AT_SHUTDOWN											//Sistemler
+#define ENABLE_OBSERVER_DAMAGE_FIX									//Sistemler
+#define ENABLE_PORT_SECURITY										//Sistemler
+#define ENABLE_GOHOME_IFNOMAP										//Sistemler
+#define ENABLE_CUBE_RENEWAL											//Sistemler
+#define ENABLE_ISIM_REAPIRACMD										//Sistemler
+#define ENABLE_GLOBAL_CHAT											//Sistemler
+#define ENABLE_REFINE_MSG_ADD										//Sistemler
+#define ENABLE_MULTISHOP											//Sistemler
+#define ITEM_CHECKINOUT_UPDATE										//Sistemler
+#define __NEW_EXCHANGE_WINDOW__										//Sistemler
+#define WJ_NEW_DROP_DIALOG											//Yeni Eþya Düþürme Penceresi
+#define ENABLE_MESSENGER_TEAM										//Sistemler
+#define __WEAPON_COSTUME_SYSTEM__									//Sistemler
+#define ENABLE_MOUNT_COSTUME_SYSTEM									//Sistemler
+#define __EFFECT_SYSTEM__											//Parlama ve Özel Efekt Sistemi.
+#define OFFLINE_SHOP												//Offline Shop Sistemi
+#define GIFT_SYSTEM													//Pazar Gift Sistemi
+#define SHOP_TIME_REFRESH 1*60										//time for cycle checking older shops
+#define SHOP_BLOCK_GAME99											//Blocking create shops on channel 99
+#define SHOP_AUTO_CLOSE												//Enable auto closing shop after sell last item
+#define SHOP_GM_PRIVILEGES GM_IMPLEMENTOR							//Minimum GM privileges to using Shop GM Panel
+#define ENABLE_CHEQUE_SYSTEM										//Won Sistemi Pack üzerinden Kapalý.
+#define ENABLE_OFFLINE_SHOP_USE_CHEQUE								//Offline Shop Pazarda Won Pack Kapalý.
+#define ENABLE_SHOP_USE_CHEQUE										//Pazarda Won Sistemi /Packten kapalý.
+#define ENABLE_SPLIT_INVENTORY_SYSTEM								//K envanteri Sistemi
+#define __SORT_INVENTORY_ITEMS__									// Sorts all inventory items
+#define __SEND_TARGET_INFO__										//Target Info Sistemi
+#define ENABLE_SEND_TARGET_INFO_EXTENDED							//Sistemler
+#define __SWITCH_CHANNEL_SYSTEM__									// TR Tipi kanal deðiþtirme sistemi
+#define __ENABLE_SHOP_DECORATION_SYSTEM__							// Kaþmir Paketi Sistemi
+#define SHOP_SEARCH													//Nesne Aram Sistemi
+#define __FULL_NOTICE_SYSTEM__										//Sistemler
+#define __HIDE_COSTUME_SYSTEM__										// Hide costume part
+#define ENABLE_AFFECT_POLYMORPH_REMOVE								//Sistemler
+#define ENABLE_SHOW_CHEST_DROP										//Sistemler
+#define WJ_TARGET_HP_SYSTEM											//Sistemler
+#define ENABLE_LEVEL_SHOW_HIDE										//Sistemler
+#define ENABLE_SOLD_ITEM_SYSTEM										// Offline Shop Satýldý Sistemi
+#define ENABLE_OFFLINESHOP_MESSAGE_SYSTEM							// Offline Shop Mesaj Gonderme Sistemi
+#define WJ_ENABLE_TRADABLE_ICON										//Sistemler
+#define ENABLE_SWITCHBOT											//Server Taraflý Efsun Botu Sistemi
+#define WJ_ELDER_ATTRIBUTE_SYSTEM									//Kadim Efsun Sistemi
+#define ENABLE_COLLECT_QUEST_SYSTEM									//Hýzlý Biyalog Sistemi
+#define ENABLE_REMOTE_SHOP											//Uzaktan Market Sistemi
+#define ENABLE_GIVE_BASIC_ITEM										// Oyun baþlangýcýnda item ver
+#define ENABLE_LONCA_TOPLANTI										// Lonca toplantý sistemi
+#define ENABLE_NEW_TYPE_OF_POTION									//Sýnýrsýz Pot ve Kutsamalar
+#define __BOSS_SECURITY__											//Boss Securýty Sistemi
+#endif
+
+//##################---HACK_FIXED---##################//
+#define HAYALET_MOD_HILESI_FIX
+#define TELEPORT_HILESI_LOG
+#define MADENCI_BOTU_HILESI_FIX
+#define BLOKLU_YERLERDE_ATAK_YAPMA_HILESI_FIX
+//##################---HACK_FIXED---##################//
+
+//######Güncel Versiyon Sistemi####///
+#define ENABLE_GCC //GCC8 / C++11 : Date Upgrade 3.12.2021
+#define ENABLE_CLIENT_VERSION_SYSTEM		// Client Version Sistemi (3.12.2021)
+//######Güncel Versiyon Sistemi####///
+
+//######Kapalý Sistem####///
+//#define __AUCTION__
+//#define ENABLE_SAFEBOX_MONEY_SYSTEM // Depoda Won ve Yang Sistemi
+//######Kapalý Sistem####///
